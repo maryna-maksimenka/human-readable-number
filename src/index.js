eb13cf646db8282829330bf13bb39472e9902fe4
maryna-maksimenka <maksimenka.maryna@gmail.com>
2020-03-18 21:07:51 +0300

    feat: implement the toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function toReadable (number) {
-  
+    units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+        
+    if (number === 0) {
+        return 'zero';
+    }
+
+    const readableNumber = []
+
+    if (number >= 100) readableNumber.push(units[number / 100 | 0], 'hundred');
+    if (number % 100 / 10 >= 2) readableNumber.push(tens[number % 100 / 10 | 0]);
+    if (number % 100 < 20) readableNumber.push(units[number % 100]);
+    else if (number % 10) readableNumber.push(units[number % 10]);
+    
+    return readableNumber.join(' ').trim();
 }
